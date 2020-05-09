c6e32268f942ddc7929e9cfbd95f57c4684476bb
Larisa Karelina <karelina.rss@gmail.com>
2020-05-09 15:41:43 +0300

    add files

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int payment = 10010;
+        int bonus;
+        if (payment >= 10010) {
+            bonus = payment / 100;
+        } else {
+            bonus = 0;
+        }
+        System.out.println("Поздравлем! Ваш бонусный баланс = " + bonus);
+    }
+}
